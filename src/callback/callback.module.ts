864a9ebfe6de5d0d3054f8feeeb45a8519204275
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-11-22 12:44:40 +0200

    add ReplyService

--- src/callback/callback.module.ts
+++ src/callback/callback.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CallbackController } from './callback.controller';
 import { CallbackService } from './callback.service';
+import { ReplyService } from './reply/reply.service';
 
 @Module({
   controllers: [CallbackController],
-  providers: [CallbackService],
+  providers: [CallbackService, ReplyService],
 })
 export class CallbackModule {}
