85d9310bb73e889522314fbed704758e8f5e2f6a
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-11-23 16:12:36 +0200

    add verify_token

--- src/callback/callback.controller.ts
+++ src/callback/callback.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, ForbiddenException, Get, Query } from '@nestjs/common';
+import { CallbackService } from './callback.service';
 
 @Controller('callback')
-export class CallbackController {}
+export class CallbackController {
+  constructor(private readonly callbackService: CallbackService) {}
+
+  @Get('/webhook')
+  getHello(
+    @Query('hub.mode') mode: string,
+    @Query('hub.verify_token') token: string,
+    @Query('hub.challenge') challenge: string,
+  ): string {
+    if (this.callbackService.verifyToken(mode, token)) {
+      return challenge;
+    }
+    throw new ForbiddenException('Access Denied');
+  }
+}
--- src/callback/callback.service.ts
+++ src/callback/callback.service.ts
@@ -3,4 +3,18 @@ import { Injectable, Logger } from '@nestjs/common';
 @Injectable()
 export class CallbackService {
   private readonly logger = new Logger(CallbackService.name);
+
+  /**
+   * UPDATE YOUR VERIFY TOKEN
+   *This will be the Verify Token value when you set up webhook
+   **/
+  verifyToken(mode, token): boolean {
+    this.logger.log('mode', mode);
+    this.logger.log('token', token);
+    if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+      // Respond with 200 OK and challenge token from the request
+      return true;
+    }
+    return false;
+  }
 }
