6c6672e12b0687cbf5f43b2a8315ce0fbd3ebe1d
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-11-23 20:59:58 +0200

    ReplyService

--- src/callback/callback.controller.ts
+++ src/callback/callback.controller.ts
@@ -8,10 +8,14 @@ import {
 } from '@nestjs/common';
 import { CallbackService } from './callback.service';
 import { NotificationPayloadDTO } from './dto/notification-payload';
+import { ReplyService } from './reply/reply.service';
 
 @Controller('callback')
 export class CallbackController {
-  constructor(private readonly callbackService: CallbackService) {}
+  constructor(
+    private readonly callbackService: CallbackService,
+    private readonly replyService: ReplyService,
+  ) {}
 
   @Get('/webhook')
   getWebhook(
@@ -28,6 +32,18 @@ export class CallbackController {
   @Post('/webhook')
   postWebhook(@Body() body: NotificationPayloadDTO): string {
     this.callbackService.create(body);
+    if (
+      body.entry &&
+      body.entry[0].changes &&
+      body.entry[0].changes[0] &&
+      body.entry[0].changes[0].value.messages &&
+      body.entry[0].changes[0].value.messages[0]
+    ) {
+      this.replyService.sendSimpleAwayMessage(
+        body.entry[0].changes[0].value.messages[0].from,
+      );
+    }
+
     return 'callback saved';
   }
 }
--- src/callback/reply/reply.service.ts
+++ src/callback/reply/reply.service.ts
@@ -9,7 +9,28 @@ export class ReplyService {
   private readonly logger = new Logger(ReplyService.name);
   constructor(private readonly httpService: HttpService) {}
 
-  sendSimpleText(): Observable<AxiosResponse<WhatsappResponse>> {
-    return this.httpService.post('http://localhost:3000/cats');
+  sendSimpleAwayMessage(
+    recipient: string,
+  ): Observable<AxiosResponse<WhatsappResponse>> {
+    this.logger.log('Sending message to ' + recipient);
+    return this.httpService.post(
+      `https://graph.facebook.com/${process.env.META_VERSION}/${process.env.PHONE_NUMBER_ID}/messages`,
+      {
+        messaging_product: 'whatsapp',
+        recipient_type: 'individual',
+        to: recipient,
+        type: 'text',
+        text: {
+          preview_url: false,
+          body: 'Hello! Thank you for reaching out. Our support team is currently away, but we will be back to assist you shortly.',
+        },
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.WA_ACCESS_TOKEN}`,
+        },
+      },
+    );
   }
 }
